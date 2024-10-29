0b5adddcc0e3aece151152003ef9586bdc8edf04
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-10-29 11:51:21 -0400

    setup app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('04 Store API')
+require('dotenv').config();
+// async errors
+
+
+const express = require('express');
+const app = express();
+
+// Middleware
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found');
+app.use(express.json());
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// Products routes
+
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connect to db
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start();
+
+
+
