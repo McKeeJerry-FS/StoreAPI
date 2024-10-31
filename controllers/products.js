900942ede1e414d7927bb8cca529cf88664e1b9f
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-10-31 11:59:51 -0400

    set up a 'company' query parameter

--- controllers/products.js
+++ controllers/products.js
@@ -6,13 +6,16 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products, nbHits: products.length });
 }
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company } = req.query;
 
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
 
+  }
+  if (company) {
+    queryObject.company = company;
   }
 
   const products = await Product.find(queryObject);
